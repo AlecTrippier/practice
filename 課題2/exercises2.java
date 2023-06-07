6c1fcf1766e61b8858c758b2a65908dbcf8416bd
Laravel <webiten3@gmail.com>
2023-06-08 08:44:28 +0900

    2回目のコミット

--- 課題2/exercises2.java
+++ 課題2/exercises2.java
@@ -3,8 +3,8 @@ public class exercises2 {
 
     public static void main(String[] args) {
 
-        int num1 = 14;
-        int num2 = 14;
+        int num1 = 8;
+        int num2 = 13;
 
         if ((num1 + num2) % 7 == 0) {
             if (num1 > 10 || num1 % 2 == 0) {
