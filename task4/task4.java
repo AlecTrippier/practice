d613136becb5cec46435ee54b11253d760526472
Laravel <webiten3@gmail.com>
2023-06-08 10:53:48 +0900

    課題4のコミット

--- task4/task4.java
+++ task4/task4.java
@@ -1,12 +1,11 @@
-package task4;
-
 public class task4 {
     int age = 20;
     String gender = "female"; // female ：女性、 male ：男性
     String status = "";
 
     public static void main(String[] args) {
-        if (age >= 20) {
+        task4 obj = new task4();
+        if (obj.age >= 20) {
             System.out.println("成人です");
         }
     }
