1b5a1ac40efc0ad3d7d5abfbd4c0872d9c291759
Laravel <webiten3@gmail.com>
2023-06-09 08:12:08 +0900

    AllayList練習問題2回目コミット

--- ArrayList/Task2/Task2.java
+++ ArrayList/Task2/Task2.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Task1 {
+public class Task2 {
     public static void main(String[] args) {
         List<String> animalList = new ArrayList<String>();
 
@@ -9,6 +9,6 @@ public class Task1 {
         animalList.add("cat");
         animalList.add("mouse");
 
-
+        animalList.set(2, "hamster");
     }
 }
