7964249269a350016613c37f8f891bd16348e159
Laravel <webiten3@gmail.com>
2023-06-08 11:07:56 +0900

    課題5コミット

---
+++ task5/task5.java
@@ -0,0 +1,12 @@
+public class task5 {
+    int age = 20;
+    static String gender = "female"; // female ：女性、 male ：男性
+    static String status = "";
+
+    public static void main(String[] args) {
+
+        if (gender == "female") {
+            System.out.println("女性です");
+        }
+    }
+}
