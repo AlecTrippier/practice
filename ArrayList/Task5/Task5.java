033e36bb808f9cfc029b8e020932940fd8139602
Laravel <webiten3@gmail.com>
2023-06-09 09:46:55 +0900

    ArrayList練習問題6回目コミット

---
+++ ArrayList/Task5/Task5.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public  class Task5{
+
+    public static void main(String[] args){
+        Map<String,String> dogDetails = new HashMap<String,String>();
+        dogDetails.put("name","コタロー");
+        dogDetails.put("type","ブルテリア");
+
+        Map<String,String> catDetails = new HashMap<String,String>();
+        catDetails.put("name","コジロー");
+        catDetails.put("type","マンチカン");
+
+        Map<String,String> hamsterDetails = new HashMap<String,String>();
+        hamsterDetails.put("name","ハムタロー");
+        hamsterDetails.put("type","キンクマ");
+
+
+        System.out.println(dogDetails.get("name")+ "(" + dogDetails.get("type") + ")");
+
+    }
+
+}
