7e26cf400ad2f6a312a4d158cd8030b6e9eb1fb1
Laravel <webiten3@gmail.com>
2023-06-10 18:17:58 +0900

    アルゴリズム練習問題1回目コミット

---
+++ Algorithm/Task1/Task1.java
@@ -0,0 +1,22 @@
+public class Kadai001 {
+
+    public static void main(String[] args) {
+        String str = "abcadefg";
+        System.out.println(isUniqueChars(str));
+    }
+
+    public static boolean isUniqueChars(String str) {
+        // 【1】
+        if (str.length() > 128) return false;
+        // 【2】
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length(); i++ ) {
+            // 【3】
+            int val = str.charAt(i);
+            // 【4】
+            if (char_set[val]) return false;
+            char_set[val] = true;
+        }
+        return true;
+    }
+}
