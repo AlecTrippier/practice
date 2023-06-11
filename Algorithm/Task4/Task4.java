386d46b6f99bcbb6868854e14e497a233b9ff5e7
Laravel <webiten3@gmail.com>
2023-06-11 16:35:32 +0900

    algorithm練習問題4回目コミット

---
+++ Algorithm/Task4/Task4.java
@@ -0,0 +1,15 @@
+public class Kadai004 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        swap(a, b);
+    }
+
+    public static void swap(int a, int b) {
+        a = a - b;
+        b = a + b;
+        a = b - a;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+}
